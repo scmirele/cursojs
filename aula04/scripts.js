7aabae42fa886c7ba47a7df5c00fc4fa35bb70a6
scmirele <mirelecn@gmail.com>
2023-05-23 17:49:01 -0300

    initial aula 06

--- aula04/scripts.js
+++ aula04/scripts.js
@@ -1,3 +1,3 @@
 alert('minha primeira mensagem eba xxD')
-confirm('tá gostando do js? yes or yes')
+confirm('tá gojs? yes or yes')
 prompt('whats your name, dev?')
---
+++ aula06/scripts002.js
@@ -0,0 +1,9 @@
+// var name = prompt('whats your name, dev?') 
+// alert('nice to meet you, ' + name +' <3')
+
+var n1 = prompt('digite um número: ')
+var n2 = prompt('digite outro número: ')
+var s = n1 + n2
+alert('A soma é: ' + s)
+
+
