8dba32c44376993bcb705df295f529b1ea113d6d
scmirele <mirelecn@gmail.com>
2023-05-23 22:47:09 -0300

    finalizando aula sobre tratamento de dados

--- aula06/scripts002.js
+++ aula06/scripts002.js
@@ -1,9 +1,13 @@
 // var name = prompt('whats your name, dev?') 
 // alert('nice to meet you, ' + name +' <3')
 
-var n1 = prompt('digite um número: ')
-var n2 = prompt('digite outro número: ')
-var s = n1 + n2
-alert('A soma é: ' + s)
+// var n1 = Number(prompt('digite um número: ')) 
+// var n2 = Number(prompt('digite outro número: ')) 
+// var s = n1 + n2
+// alert('A soma é: ' + s)
+// alert(`A soma entre ${n1} + ${n2} é igual ${s}`)
 
-
+var name = prompt('whats your name?') 
+document.writeln(`hello, ${name}! your name have ${name.length} letters.`)
+document.writeln(`your name in uppercase is ${name.toUpperCase()}.`)
+document.writeln(`your name in lowercase is ${name.toLowerCase()}.`)
